8803d1ea8beb0fa8d9094cfcb8c019195be412c3
jecausey <50413692+jecausey@users.noreply.github.com>
2021-11-15 11:06:11 -0600

    Delete tfstate.tf

--- tfstate.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-state-bucket-jhc"
-    key            = "ec2/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-  }
-}
